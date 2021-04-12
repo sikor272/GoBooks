1019ba8b6377a5e9332bf7e43c00ca23e03ff576
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-04-12 15:51:47 +0200

    Metody equals() i hashCode() dla encji tokenu

--- src/main/java/pl/umk/mat/gobooks/auth/ApiLoginEntry.java
+++ src/main/java/pl/umk/mat/gobooks/auth/ApiLoginEntry.java
@@ -3,6 +3,7 @@ package pl.umk.mat.gobooks.auth;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import pl.umk.mat.gobooks.common.BaseEntity;
@@ -10,6 +11,7 @@ import pl.umk.mat.gobooks.users.User;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
@@ -20,6 +22,7 @@ import java.time.Instant;
 @SQLDelete(sql = "UPDATE users SET deleted = true WHERE id = ?")
 public class ApiLoginEntry extends BaseEntity {
 
+    @NaturalId
     @Column(unique = true, nullable = false)
     private String token;
 
@@ -34,5 +37,19 @@ public class ApiLoginEntry extends BaseEntity {
         this.user = user;
         this.token = token;
         this.expiredAt = expiredAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ApiLoginEntry)) return false;
+        if (!super.equals(o)) return false;
+        ApiLoginEntry that = (ApiLoginEntry) o;
+        return getToken().equals(that.getToken());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getToken());
     }
 }
