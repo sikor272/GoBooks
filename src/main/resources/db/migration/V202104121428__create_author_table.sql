a5c60c8ff0396236636ca1bee28a65cc7ea439dc
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-04-12 15:19:34 +0200

    Usunięcie zbędnego pola UUID

--- src/main/resources/db/migration/V202104121428__create_author_table.sql
+++ src/main/resources/db/migration/V202104121428__create_author_table.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS authors;
 CREATE TABLE authors
 (
     id          BIGSERIAL PRIMARY KEY,
-    uuid        VARCHAR(36)  NOT NULL UNIQUE,
     first_name  VARCHAR(255) NOT NULL,
     last_name   VARCHAR(255) NOT NULL,
     nationality VARCHAR(255),
