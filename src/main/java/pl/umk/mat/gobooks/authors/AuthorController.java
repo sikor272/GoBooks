f9244e45790ad2151c329e312e0f4b2a33105d5c
sikor272 <dawidsikorski272@gmail.com>
2021-05-20 22:19:40 +0200

    Dodanie adnotacji swaggera

--- src/main/java/pl/umk/mat/gobooks/authors/AuthorController.java
+++ src/main/java/pl/umk/mat/gobooks/authors/AuthorController.java
@@ -1,5 +1,7 @@
 package pl.umk.mat.gobooks.authors;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -15,21 +17,25 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("api/authors")
 @RequiredArgsConstructor
+@Tag(name = "Author Controller", description = "This controller provide logic for authors.")
 public class AuthorController {
 
     private final AuthorService authorService;
 
     @GetMapping
+    @Operation(summary = ".", tags = {"Author Controller"})
     public IterableResponse<AuthorResponse> getAll(Pageable pageable) {
         return new IterableResponse<>(authorService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = ".", tags = {"Author Controller"})
     public AuthorResponse getById(@PathVariable Long id) {
         return authorService.findById(id);
     }
 
     @GetMapping("/search")
+    @Operation(summary = ".", tags = {"Author Controller"})
     public IterableResponse<AuthorResponse> search(
             @RequestParam(defaultValue = "") String firstName,
             @RequestParam(defaultValue = "") String lastName,
@@ -40,18 +46,21 @@ public class AuthorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = ".", tags = {"Author Controller"})
     public AuthorResponse save(@RequestBody @Valid NewAuthor newAuthor)
             throws BadRequest {
         return authorService.save(newAuthor);
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = ".", tags = {"Author Controller"})
     public AuthorResponse updateBiography(@PathVariable Long id, @RequestBody @Valid AuthorBiography biography) {
         return authorService.updateBiography(id, biography);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    @Operation(summary = ".", tags = {"Author Controller"})
+    public void delete(@PathVariable Long id) throws BadRequest {
         authorService.delete(id);
     }
 }
--- src/main/java/pl/umk/mat/gobooks/authors/dto/AuthorResponse.java
+++ src/main/java/pl/umk/mat/gobooks/authors/dto/AuthorResponse.java
@@ -1,18 +1,27 @@
 package pl.umk.mat.gobooks.authors.dto;
 
 import com.neovisionaries.i18n.CountryCode;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import pl.umk.mat.gobooks.authors.Author;
 
 @Getter
+@ApiModel
 public class AuthorResponse {
-
+    @ApiModelProperty(notes = "It is exactly what you expect.")
     private final String firstName;
+    @ApiModelProperty(notes = "It is exactly what you expect.")
     private final String lastName;
+    @ApiModelProperty(notes = "It is exactly what you expect.")
     private final CountryCode nationality;
+    @ApiModelProperty(notes = "It is exactly what you expect.")
     private final String biography;
+    @ApiModelProperty(notes = "It's exactly what you expect.")
+    private final Long id;
 
     public AuthorResponse(Author author) {
+        this.id = author.getId();
         this.firstName = author.getFirstName();
         this.lastName = author.getLastName();
         this.nationality = author.getNationality();
