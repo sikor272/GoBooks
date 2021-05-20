8c9c33a39f38dfc6fe62bc95df0ea14bf124d7de
sikor272 <dawidsikorski272@gmail.com>
2021-05-20 22:20:06 +0200

    Logika wydawnictwa

--- src/main/java/pl/umk/mat/gobooks/publisher/PublishingHouseController.java
+++ src/main/java/pl/umk/mat/gobooks/publisher/PublishingHouseController.java
@@ -1,2 +1,70 @@
-package pl.umk.mat.gobooks.publisher;public class PublishingHouseController {
+package pl.umk.mat.gobooks.publisher;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pl.umk.mat.gobooks.commons.IterableResponse;
+import pl.umk.mat.gobooks.commons.exceptions.BadRequest;
+import pl.umk.mat.gobooks.commons.exceptions.ResourceAlreadyExist;
+import pl.umk.mat.gobooks.publisher.dto.NewPublishingHouse;
+import pl.umk.mat.gobooks.publisher.dto.PublishingHouseResponse;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/publishinghouse")
+@RequiredArgsConstructor
+@Tag(name = "Publishing House Controller", description = "This controller provide logic for publishing house.")
+public class PublishingHouseController {
+    private final PublishingHouseService publishingHouseService;
+
+    @GetMapping
+    @Operation(summary = ".", tags = {"Publishing House Controller"})
+    public IterableResponse<PublishingHouseResponse> getAll(Pageable pageable) {
+        return new IterableResponse<>(publishingHouseService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = ".", tags = {"Publishing House Controller"})
+    public PublishingHouseResponse getById(@PathVariable Long id) {
+        return publishingHouseService.findById(id);
+    }
+
+    @GetMapping("/search")
+    @Operation(summary = ".", tags = {"Publishing House Controller"})
+    public IterableResponse<PublishingHouseResponse> search(
+            @RequestParam(defaultValue = "") String name,
+            Pageable pageable
+    ) {
+        return new IterableResponse<>(publishingHouseService.search(name, pageable));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = ".", tags = {"Publishing House Controller"})
+    public PublishingHouseResponse save(
+            @RequestBody @Valid NewPublishingHouse newPublishingHouse
+    ) throws BadRequest {
+        return publishingHouseService.save(newPublishingHouse);
+    }
+
+    @PatchMapping("/{id}")
+    @Operation(summary = ".", tags = {"Publishing House Controller"})
+    public PublishingHouseResponse updateName(
+            @PathVariable Long id,
+            @RequestBody @Valid NewPublishingHouse newPublishingHouse
+    ) throws ResourceAlreadyExist {
+        return publishingHouseService.updateName(id, newPublishingHouse);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = ".", tags = {"Publishing House Controller"})
+    public void delete(@PathVariable Long id) throws BadRequest {
+        publishingHouseService.delete(id);
+    }
+
+
 }
--- src/main/java/pl/umk/mat/gobooks/publisher/dto/NewPublishingHouse.java
+++ src/main/java/pl/umk/mat/gobooks/publisher/dto/NewPublishingHouse.java
@@ -1,2 +1,15 @@
-package pl.umk.mat.gobooks.publisher.dto;public class NewPublishingHouse {
+package pl.umk.mat.gobooks.publisher.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@ApiModel
+@Getter
+public class NewPublishingHouse {
+    @NotBlank(message = "Name of publishing house can not be empty")
+    @ApiModelProperty(notes = "It is exactly what you expect.")
+    private String name;
 }
