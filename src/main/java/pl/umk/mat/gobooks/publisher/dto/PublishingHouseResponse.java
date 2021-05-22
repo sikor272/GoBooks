ea2c4dd266b03e3aed9dba6f4bf09a5b39044681
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-05-22 11:27:53 +0200

    Brakujące gettery w DTO

--- src/main/java/pl/umk/mat/gobooks/publisher/dto/PublishingHouseResponse.java
+++ src/main/java/pl/umk/mat/gobooks/publisher/dto/PublishingHouseResponse.java
@@ -2,8 +2,10 @@ package pl.umk.mat.gobooks.publisher.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
 import pl.umk.mat.gobooks.publisher.PublishingHouse;
 
+@Getter
 @ApiModel
 public class PublishingHouseResponse {
     @ApiModelProperty(notes = "It is exactly what you expect.")
